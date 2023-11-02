8604edb796ea36e99ebca295b8b23abcb4e90cfa
SUHA ALHABAJ <40003169+sssalcor@users.noreply.github.com>
2023-11-02 11:04:13 -0500

    Update streamlit_app.py
    add more items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit
 
 streamlit.title('My parents new healthy dinner')
+
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast ')
 
